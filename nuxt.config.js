d330307a29bb2c4d5e3c60e5c01008b250cacd6f
Alexandr Borecky <alexandrborecky@Alexandrs-MacBook-Pro.local>
2021-01-08 16:08:50 +0100

    font fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Mulish:wght@400;500;600;700;800;900&display=swap' },
-      {rel: "preconnect", href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,600;0,700;0,800;0,900;1,400&display=swap", rel: "stylesheet">
+      {rel: "preconnect", href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,600;0,700;0,800;0,900;1,400&display=swap", rel: "stylesheet"}
     ]
   },
   /*
