bed4591778e9cbd9ec7f9d21e01d7231de5b1ecc
Alexandr Borecky <alexandrborecky@Alexandrs-MacBook-Pro.local>
2021-05-04 16:59:42 +0200

    button fix

--- plugins/uiKit.js
+++ plugins/uiKit.js
@@ -6,7 +6,7 @@ import sideBar from '@/components/sideBar.vue'
 import testComponent from '@/components/testComponent.vue'
 import filmPremiere from '@/components/filmPremiere.vue'
 
-import PrimaryButton from '~/components/buttons/PrimaryButton.vue'
+import PrimaryButton from '@/components/buttons/PrimaryButton.vue'
 import secondaryButton from '@/components/buttons/secondaryButton.vue'
 import textButton from '@/components/buttons/textButton.vue'
 import externalButton from '@/components/buttons/externalButton.vue'
